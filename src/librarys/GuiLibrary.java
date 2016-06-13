21bf198ae3b642c1f10b68805a2a8888aed24e17
ajparker256 <parkeraj256@gmail.com>
2016-06-12 23:17:59 -0700

    committing

--- src/librarys/GuiLibrary.java
+++ src/librarys/GuiLibrary.java
@@ -7,7 +7,6 @@ public static GuiTexture button1;
 public static GuiTexture gun;
 public static GuiTexture crosshair;
 public static int bulletTexture;
-
 public static int background;
 public static int shop;
 public static int nextWave;
---
+++ src/librarys/TextureLibrary.java
@@ -0,0 +1,15 @@
+package librarys;
+
+public class TextureLibrary {
+public static int Num0;
+public static int Num1;
+public static int Num2;
+public static int Num3;
+public static int Num4;
+public static int Num5;
+public static int Num6;
+public static int Num7;
+public static int Num8;
+public static int Num9;
+
+}
