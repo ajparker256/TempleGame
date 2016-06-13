8174b036124d6f2adc41150746e708d6b0345f72
notWizro <fourthbit@gmail.com>
2016-06-12 23:17:42 -0700

    did some thingies

---
+++ src/grid/Void.java
@@ -0,0 +1,19 @@
+package grid;
+
+import org.lwjgl.util.vector.Vector2f;
+
+
+//This is the most basic tile, it's empty and completely passable. This tile replaces other tiles when
+//they get removed
+
+public class Void extends Tile{
+	
+	
+	public Void(Vector2f loc) {
+		super(loc);
+		this.passable = true;
+		this.hp = 0;
+		init();
+	}
+
+}
