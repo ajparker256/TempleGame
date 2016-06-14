1e655f6a8b899a489908f2d67fa4faef0dd3e531
Jackson Mills <Jackson Mills>
2016-06-14 16:51:01 -0700

    Added a button, it has a hitbox and can test if the mouse is within its borders if given mouse X and mouse Y

---
+++ src/gui/Button.java
@@ -0,0 +1,45 @@
+package gui;
+
+import org.lwjgl.util.vector.Vector2f;
+
+public class Button {
+	//This is a list of the top left corners of hitboxes
+		private Vector2f hitBoxesTL;
+		
+		//This is a list of the Bottom Right corners of the hitboxes
+		private Vector2f hitBoxesBR;
+		
+		//This is the image that displays for the button
+		private GuiTexture design;
+		
+		public Button(Vector2f TopLeft, Vector2f BottomRight, GuiTexture g) {
+			hitBoxesTL = TopLeft;
+			hitBoxesBR = BottomRight;
+			design = g;
+		}
+		
+		public void setTL(Vector2f tl) {
+			hitBoxesTL = tl;
+		}
+		
+		public void setBR(Vector2f br) {
+			hitBoxesBR = br;
+		}
+				
+		public Vector2f getTL() {
+			return hitBoxesTL;
+		}
+		
+		public GuiTexture getDesign() {
+			return design;
+		}
+		
+		public Vector2f getBR() {
+			return hitBoxesBR;
+		}
+		
+		//Returns true if the mouseX and mouseY are within the confines of the button
+		public boolean isClicked(int x, int y) {
+			return (x>hitBoxesTL.x && x<hitBoxesBR.x && y>hitBoxesTL.y && y<hitBoxesBR.y);
+		}
+}
