1d426e14530a183b5363feb37065c084255ba02d
notWizro <fourthbit@gmail.com>
2016-06-13 23:14:29 -0700

    MEMES

--- src/grid/PitfallTrap.java
+++ src/grid/PitfallTrap.java
@@ -1,25 +1,33 @@
 package grid;
 
 import org.lwjgl.util.vector.Vector2f;
+
+import entities.Unit;
+
 import java.util.ArrayList;
 
 public class PitfallTrap extends Tile{
+	
+	private final int HEATDEATHOFTHEUNIVERSE;
 
 	public PitfallTrap(Vector2f loc, Grid gr) {
 		super(loc, gr);
 		this.passable = true;
 		this.hp = 0;
 		init();
+		HEATDEATHOFTHEUNIVERSE = 3; // this is the amount of units needed on the trap to trigger it
 	}
 	
 	public void update(){
-		if(unitsContained.size() >= 3)
+		if(unitsContained.size() >= HEATDEATHOFTHEUNIVERSE)
 			trigger();
 	}
 	
 	public void trigger(){
-		
-		
+		for(Unit u: unitsContained){
+			//KILL THE UNIT IN HERE<<<<<<<<<<<<<<<<<<<<<<----------------
+		}
+		this.unitsContained.clear();
 	}
 	
 }
