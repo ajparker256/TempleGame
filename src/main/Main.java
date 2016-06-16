820f57c3f0dbfa5222f5ce78b6d610b10d655996
Jackson Mills <Jackson Mills>
2016-06-16 16:01:13 -0700

    Bottom left corner for the grid

--- src/main/Main.java
+++ src/main/Main.java
@@ -49,7 +49,7 @@ public static long milli;
 		Loader loader = new Loader();
 		Random random = new Random();
 		new TextureLibrary(loader);
-		Grid grid= new Grid(new Vector2f(0f,-1),0.05f,10);
+		Grid grid= new Grid(new Vector2f(-.5f,-.8f),0.025f,20);
 
 		
 		
