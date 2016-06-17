7c157fe8d707490c87d8c20c0929e509247bdb49
Jackson Mills <Jackson Mills>
2016-06-16 22:32:01 -0700

    s

---
+++ src/buttons/Shop.java
@@ -0,0 +1,10 @@
+package buttons;
+
+public class Shop {
+	
+	//Button array to determine which spot is being interacted with.
+	Button[] buttons;
+	
+	//
+
+}
--- src/grid/Tile.java
+++ src/grid/Tile.java
@@ -21,15 +21,22 @@ public class Tile {
 	protected Vector2f position;
 	private int texture;
 	private GuiTexture guiTexture;
+	private String name;
 	
 	//creates a tile in location loc, give location in column then row
 	public Tile(int x, int y, float size, Vector2f location){
 		this.position=new Vector2f (location.x+((x-size/2)*size*2),(float) (location.y+((y-size/2)*(size*2*DisplayManager.getAspectratio()))));
 		this.texture=1;
 		this.guiTexture=(new GuiTexture(TextureLibrary.getTile(texture),position,new Vector2f(size,(float) (size*DisplayManager.getAspectratio()))));	
-		}
+		name = "Default_Name";
+	}
+	
 	public GuiTexture drawTile(){
 		return guiTexture;
+	}
+	
+	public String getName() {
+		return name;
 	}
 	
 
