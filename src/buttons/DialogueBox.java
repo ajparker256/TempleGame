11a3eb20955e854b2b68b1b776a4caeba41540b2
Jackson Mills <Jackson@Jacksons_PC.(none)>
2016-07-13 23:38:29 -0400

    Dialogue box beta out

--- src/buttons/DialogueBox.java
+++ src/buttons/DialogueBox.java
@@ -18,9 +18,20 @@ public class DialogueBox extends Shop{
 	
 	private String message;
 	
-	public DialogueBox(Vector2f location, Vector2f size, Tile[][] options, String message) {
+	private Shop shop;
+	
+	public DialogueBox(Vector2f location, Vector2f size, Tile[][] options, String message, Shop s) {
 		super(location, size, options);
+		this.shop = s;
 		this.message = message;
+		confirm = new Button(new Vector2f(location.x,
+				location.y+StringLibrary.getSize().y),
+				new Vector2f(location.x+StringLibrary.getWidth("Confirm"),
+						location.y));
+		cancel = new Button(new Vector2f(location.x+size.x*(.5f * (visibilityRange[1]-visibilityRange[0]))-StringLibrary.getWidth("Cancel"),
+				location.y+StringLibrary.getSize().y), 
+				new Vector2f(location.x+size.x*(.5f * (visibilityRange[1] - visibilityRange[0])),
+					location.y));
 		visibilityRange[1] = 2;
 		visibilityRange[3] = 2;
 		if(options.length<2) {
@@ -39,6 +50,17 @@ public class DialogueBox extends Shop{
 		
 	}
 	
+	public void isCanceled(float mouseX, float mouseY) {
+		if(cancel.isClicked(mouseX,mouseY)) {
+			isOn = false;
+			shop.setOn(false);
+		}
+	}
+	
+	public boolean isConfirmed(float mouseX, float mouseY) {
+		return confirm.isClicked(mouseX, mouseY);
+	}
+	
 	public void render(ArrayList<GuiTexture> guis) {
 		
 		for(int i = visibilityRange[2]; i<visibilityRange[3]; i++) {
@@ -52,12 +74,12 @@ public class DialogueBox extends Shop{
 		}
 		guis.addAll(StringLibrary.makeItFitC(message, new Vector2f(location.x, location.y+size.y+.1f), size.x));
 		
-		guis.addAll(StringLibrary.drawString("X", new Vector2f(location.x+size.x*(.5f * (visibilityRange[1]-visibilityRange[0]))-StringLibrary.getWidth('X'),
+		guis.addAll(StringLibrary.drawString("X", new Vector2f(location.x+size.x*(.5f * (visibilityRange[1]-visibilityRange[0]))-StringLibrary.getWidth("X"),
 				location.y+size.y*(.5f * (visibilityRange[3]-visibilityRange[2]))+StringLibrary.getSize().y)));
-		guis.addAll(StringLibrary.drawString("X", new Vector2f(location.x+size.x*(.5f * (visibilityRange[1]-visibilityRange[0]))-StringLibrary.getWidth('X'),
-				location.y+size.y*(.5f * (visibilityRange[3]-visibilityRange[2]))+StringLibrary.getSize().y)));
-		guis.addAll(StringLibrary.drawString("X", new Vector2f(location.x+size.x*(.5f * (visibilityRange[1]-visibilityRange[0]))-StringLibrary.getWidth('X'),
-				location.y+size.y*(.5f * (visibilityRange[3]-visibilityRange[2]))+StringLibrary.getSize().y)));
+		guis.addAll(StringLibrary.drawString("Confirm", new Vector2f(location.x,
+				location.y+StringLibrary.getSize().y)));
+		guis.addAll(StringLibrary.drawString("Cancel", new Vector2f(location.x+size.x*(.5f * (visibilityRange[1]-visibilityRange[0]))-StringLibrary.getWidth("Cancel"),
+				location.y+StringLibrary.getSize().y)));
 	}
 
 	
